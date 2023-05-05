f7e7ba8abe895776b75ad821535ffef521095690
Lourdes <Lourdes@DESKTOP-C85ANTK>
2023-05-05 14:03:14 -0600

    Primer ejemplo

---
+++ src/sistemainventario/Bebidas.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemainventario;
+
+/**
+ *
+ * @author Lourdes
+ */
+public class Bebidas {
+    
+    private String NombreBebidas;
+    private int PrecioBebidas;
+    private int Tamaño;
+
+    public String getNombreBebidas() {
+        return NombreBebidas;
+    }
+
+    public void setNombreBebidas(String NombreBebidas) {
+        this.NombreBebidas = NombreBebidas;
+    }
+
+    public int getPrecioBebidas() {
+        return PrecioBebidas;
+    }
+
+    public void setPrecioBebidas(int PrecioBebidas) {
+        this.PrecioBebidas = PrecioBebidas;
+    }
+
+    public int getTamaño() {
+        return Tamaño;
+    }
+
+    public void setTamaño(int Tamaño) {
+        this.Tamaño = Tamaño;
+    }
+
+    public Bebidas(String NombreBebidas, int PrecioBebidas, int Tamaño) {
+        this.NombreBebidas = NombreBebidas;
+        this.PrecioBebidas = PrecioBebidas;
+        this.Tamaño = Tamaño;
+    }
+
+    @Override
+    public String toString() {
+        return "Bebidas: " + "NombreBebidas=" + NombreBebidas + ", PrecioBebidas=" + PrecioBebidas + ", Tama\u00f1o=" + Tamaño + '}';
+    }
+    
+    
+    
+}
---
+++ src/sistemainventario/Comida.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemainventario;
+
+/**
+ *
+ * @author Lourdes
+ */
+public class Comida {
+    private String NombreComida;
+    private int PrecioComida;
+    private String EstadoVencimiento;
+
+    public String getNombreComida() {
+        return NombreComida;
+    }
+
+    public void setNombreComida(String NombreComida) {
+        this.NombreComida = NombreComida;
+    }
+
+    public int getPrecioComida() {
+        return PrecioComida;
+    }
+
+    public void setPrecioComida(int PrecioComida) {
+        this.PrecioComida = PrecioComida;
+    }
+
+    public String getEstadoVencimiento() {
+        return EstadoVencimiento;
+    }
+
+    public void setEstadoVencimiento(String EstadoVencimiento) {
+        this.EstadoVencimiento = EstadoVencimiento;
+    }
+
+    public Comida(String NombreComida, int PrecioComida, String EstadoVencimiento) {
+        this.NombreComida = NombreComida;
+        this.PrecioComida = PrecioComida;
+        this.EstadoVencimiento = EstadoVencimiento;
+    }
+
+    @Override
+    public String toString() {
+        return "\n Comidas: " + "\n NombreComida= " + NombreComida + "\n Precio= " + PrecioComida + "\n Estado de Vencimiento= " + EstadoVencimiento;
+    }
+    
+    
+    
+}
